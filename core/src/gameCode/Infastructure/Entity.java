23f75d7da184be6289fc2007828baea378c425e8
cte64 <colintaylorevans@gmail.com>
2020-06-30 21:29:31 -0700

    a

--- core/src/gameCode/Infastructure/Entity.java
+++ core/src/gameCode/Infastructure/Entity.java
@@ -1,36 +1,45 @@
 package gameCode.Infastructure;
 import java.util.ArrayList;
 
+
 public class Entity {
 
     public float x_pos, y_pos, angle, scaleW, scaleH, velMag, velAng, lastGoodX, lastGoodY, lastGoodAngle;
-    int bitMapX, bitMapY, z_pos, deleteRange, spriteOffsetX, spriteOffsetY;// -2 = false; -1 = delete at edge, > 0 =  delete out of range
+    public int bitMapX, bitMapY, z_pos, deleteRange, spriteOffsetX, spriteOffsetY;// -2 = false; -1 = delete at edge, > 0 =  delete out of range
     public boolean moveable, drawable, markForDeletion, flip, cameraBound;
     public String spriteName, drawMode, entityName;
     ArrayList<Component> components;
 
-
     public Entity() {
-
         x_pos = 0.0f;
         y_pos = 0.0f;
-
-
-        /*
-        public float x_pos, y_pos, angle, scaleW, scaleH, velMag, velAng, lastGoodX, lastGoodY, lastGoodAngle;
-        int bitMapX, bitMapY, z_pos, deleteRange, spriteOffsetX, spriteOffsetY;// -2 = false; -1 = delete at edge, > 0 =  delete out of range
-        public boolean moveable, drawable, markForDeletion, flip, cameraBound;
-        public String spriteName, drawMode, entityName;
-        ArrayList<Component> components;*/
-
-
-
-
-
-
+        angle = 0.0f;
+        scaleW = 0.0f;
+        scaleH = 0.0f;
+        velMag = 0.0f;
+        velAng = 0.0f;
+        lastGoodX = 0.0f;
+        lastGoodY = 0.0f;
+        lastGoodAngle = 0.0f;
+        bitMapX = 0;
+        bitMapY = 0;
+        z_pos = 0;
+        deleteRange = 0;
+        spriteOffsetX = 0;
+        spriteOffsetY = 0;
+        moveable = false;
+        drawable = false;
+        markForDeletion = false;
+        flip = false;
+        cameraBound = false;
+        spriteName = "";
+        drawMode = "";
+        entityName = "";
+        components = new ArrayList<Component>();
     }
 
     public void addComponent(Component newComponent) { components.add(newComponent); }
+
     public void updateType(String type) {
         for(Component comp: components) {
             if(comp.type == type)
@@ -38,8 +47,12 @@ public class Entity {
         }
     }
 
-    public void getComponentByName() {
-
+    public Component getComponent(String type) {
+        for(Component comp: components) {
+            if(comp.type == type)
+                return comp;
+        }
+        return null;
     }
 
     public int getWidth() {
@@ -50,7 +63,53 @@ public class Entity {
         return 10;
     }
 
-    public float getXVelocity() { return 0.0f; }
-    public float getYVelocity() { return 0.0f; }
+
+    public void deleteComponents() { components.clear(); }
+
+    public float getXVelocity() {
+        return velMag * Math.cos( toRad(velAng) );
+    }
+
+    public float getYVelocity() {
+        return -velMag * sin( toRad(velAng) );
+    }
+
+
+    /*
+        int getWidth();
+    int getHeight();
+
+    Entity();
+    Entity(bool new_moveable, float new_x_pos, float new_y_pos, int new_z_pos, float new_x_velo, float new_y_velo);
+
+    template<typename C> void addComponent(C* c){ components[&typeid(*c)] = c;}
+    template<typename B> void deleteComponent(B* c){ delete(components[&typeid(*c)]);components.erase(&typeid(*c)); }
+    template <typename A> A* get_component() {
+        for(auto iter = components.begin(); iter != components.end(); iter++) if(iter->first == &typeid(A)) return (A*)iter->second;
+        return nullptr;
+    }
+    void update_logic(std::string update_what);
+    void interactWith(Entity& entity, std::string message);
+
+    void accelerate(float mag, float ang);
+    float getXVelocity();
+    float getYVelocity();
+    void setXVelocity(float newX);
+    void setYVelocity(float newY);
+
+    void collider();
+
+    std::string getSerializedEntity();
+
+
+    struct by_z_pos {
+        bool operator()(Entity *const& a, Entity *const& b) {
+            return (a->z_pos < b->z_pos);
+        }
+    };
+
+    Entity& operator = (const Entity &a);
+     */
+
 
 }
--- core/src/gameCode/Utilities/MathUtils.java
+++ core/src/gameCode/Utilities/MathUtils.java
@@ -2,6 +2,28 @@ package gameCode.Utilities;
 
 public class MathUtils {
 
+
+    /*
+    float angleBetweenCells(float x1, float y1, float x2, float y2);
+    float toRad(float degree);
+    float toDeg(float radian);
+     */
+
+    public static final float PI = 3.14159265359f;
+
+    public static float angle1(float x, float y) {
+        float angle = (float)(Math.atan2(y, x)*180.0/PI);
+        if(angle < 0) angle += 360.0;
+        return angle;
+    }
+
+    public static float mag(float x1, float y1, float x2, float y2) {
+        float x = x2 - x1;
+        float y = y2 - y1;
+        float mag = (float)Math.sqrt( Math.pow(x, 2.0) + Math.pow(y, 2.0) );
+        return mag;
+    }
+
     public static <A extends Comparable<A>> A clamp(A val, A min, A max) {
         if (val.compareTo(min) < 0) val = min;
         if (val.compareTo(max) > 0) val = max;
