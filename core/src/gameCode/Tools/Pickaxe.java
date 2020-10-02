248c7395b3f3c8a84d7ffe16d70b356afb23e48f
cte64 <colintaylorevans@gmail.com>
2020-10-01 23:45:24 -0700

    stuff

---
+++ core/src/gameCode/Tools/Pickaxe.java
@@ -0,0 +1,20 @@
+package gameCode.Tools;
+
+import gameCode.Infrastructure.Component;
+import gameCode.Infrastructure.Entity;
+
+public class Pickaxe extends Component {
+
+
+
+    public Pickaxe() {
+
+    }
+
+
+    public void update(Entity entity) {
+
+    }
+
+
+}
